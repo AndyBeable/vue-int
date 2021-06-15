09a50d68cd4bc4f886d6d84075ed4e55700650ed
AndyBeable <andy.beable@gmail.com>
2021-06-15 14:52:42 +0100

    list render

--- script.js
+++ script.js
@@ -5,6 +5,28 @@ var app = new Vue({
     image: './assets/socks.jpg',
     inStock: false,
     onSale: false,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
     // inventory: 111,
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+      },
+      {
+        variantId: 22345,
+        variantColor: 'blue',
+      },
+    ],
+    sizes: [
+      {
+        size: 'small',
+      },
+      {
+        size: 'medium',
+      },
+      {
+        size: 'large',
+      },
+    ],
   },
 });
