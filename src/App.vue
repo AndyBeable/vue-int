df658c8b1a1f0ed63d23d1c42dd6ee3839eb5a95
AndyBeable <andy.beable@gmail.com>
2021-06-21 15:29:31 +0100

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,15 @@
 <template>
   <div id="app">
-    {{ user.username }} - {{ fullName }}
-    <strong>Followers:</strong>
-    {{ followers }}
-    <button @click="followUser">Follow</button>
+    <UserProfile />
   </div>
 </template>
 
 <script>
+import UserProfile from './components/UserProfile';
+
 export default {
   name: 'App',
-  data() {
-    return {
-      followers: 0,
-      user: {
-        id: 1,
-        username: '@Beabs19',
-        firstName: 'Andy',
-        lastName: 'Beable',
-        email: 'andy.beable@gmail.com',
-        isAdmin: true,
-      },
-    };
-  },
-  watch: {
-    followers(newFollowerCount, oldFollowerCount) {
-      if (oldFollowerCount < newFollowerCount) {
-        console.log(`${this.user.username} has gained a follower`);
-      }
-    },
-  },
-  computed: {
-    fullName() {
-      return `${this.user.firstName} ${this.user.lastName}`;
-    },
-  },
-  methods: {
-    followUser() {
-      this.followers++;
-    },
-  },
-  mounted() {
-    this.followUser();
-  },
+  components: { UserProfile },
 };
 </script>
 
